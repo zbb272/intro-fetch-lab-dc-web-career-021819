b7f9b779131c289d699fa8a4ebc245f416ee427a
Rob Cobb <rwcobbjr@gmail.com>
2018-10-01 14:59:04 -0400

    added chai-sinon, added first stub fetch test

--- swapi.js
+++ swapi.js
@@ -1,41 +1,43 @@
-document.addEventListener('DOMContentLoaded', () => {
-  crawlButton = document.querySelector('#crawlBtn')
-  crawlButton.addEventListener('click', getOpeningCrawl)
-  planetSelector = document.querySelector('#planetForm')
-  planetSelector.addEventListener('submit', getPlanet)
-  getDroids()
-})
+document.addEventListener("DOMContentLoaded", () => {
+  crawlButton = document.querySelector("#crawlBtn");
+  crawlButton.addEventListener("click", getOpeningCrawl);
+  planetSelector = document.querySelector("#planetForm");
+  planetSelector.addEventListener("submit", getPlanet);
+  getDroids();
+});
 
 const getOpeningCrawl = () => {
-  fetch('https://swapi.co/api/films/1/')
+  fetch("https://swapi.co/api/films/1/")
     .then(r => r.json())
     .then(d => {
-      crawlDiv = document.getElementById('crawlDiv')
-      crawlDiv.innerText = d.opening_crawl
-    })
-}
+      crawlDiv = document.getElementById("crawlDiv");
+      crawlDiv.innerText = d.opening_crawl;
+    });
+};
 
 const getPlanet = e => {
-  e.preventDefault()
-  currentPlanet = parseInt(document.querySelector('#planetInput').value)
-  planetData = document.getElementById('planetData')
+  e.preventDefault();
+  currentPlanet = parseInt(document.querySelector("#planetInput").value);
+  planetData = document.getElementById("planetData");
   if (isNaN(currentPlanet)) {
-    planetData.innerText = 'please enter a number between 1 and 60'
+    planetData.innerText = "please enter a number between 1 and 60";
   } else if (currentPlanet < 1 || currentPlanet > 60) {
-    planetData.innerText = 'please enter a number between 1 and 60'
+    planetData.innerText = "please enter a number between 1 and 60";
   } else {
     fetch(`https://swapi.co/api/planets/${currentPlanet}/`)
       .then(r => r.json())
       .then(d => {
-        planetData.innerHTML = `<p>Name: ${d.name}</p> <p>Climate: ${d.climate}`
-      })
+        planetData.innerHTML = `<p>Name: ${d.name}</p> <p>Climate: ${
+          d.climate
+        }`;
+      });
   }
-}
+};
 
 const getDroids = () => {
-  let droidIDs = [2, 3]
+  let droidIDs = [2, 3];
   droidIDs.map(id => {
-    let droidDiv = document.getElementById(`droid-${id}`)
+    let droidDiv = document.getElementById(`droid-${id}`);
 
     fetch(`https://swapi.co/api/people/${id}/`)
       .then(r => r.json())
@@ -44,17 +46,17 @@ const getDroids = () => {
           <p><strong>Height:</strong> ${droid.height}</p>
           <p><strong>Mass:</strong> ${droid.mass}</p>
           <p><span class='home-planet'></span></p></br>
-          <button>Show Homeworld Details</button>`
-        let button = droidDiv.querySelector('button')
-        button.addEventListener('click', function() {
+          <button>Show Homeworld Details</button>`;
+        let button = droidDiv.querySelector("button");
+        button.addEventListener("click", function() {
           fetch(droid.homeworld)
             .then(r => r.json())
             .then(planet => {
               droidDiv.querySelector(
                 `.home-planet`
-              ).innerHTML = `<strong>Home World:</strong> ${planet.name}`
-            })
-        })
-      })
-  })
-}
+              ).innerHTML = `<strong>Home World:</strong> ${planet.name}`;
+            });
+        });
+      });
+  });
+};
--- test/indexTest.js
+++ test/indexTest.js
@@ -1,95 +1,119 @@
-const expect = chai.expect
+var expect = chai.expect;
 
-describe('swapi.js', () => {
-  describe('Opening Crawl', () => {
+describe("swapi.js", () => {
+  beforeEach(() => {
+    sinon.stub(window, "fetch");
+  });
+
+  afterEach(() => {
+    window.fetch.restore();
+  });
+
+  describe("Opening Crawl", () => {
+    let fakeData = { opening_crawl: "test opening crawl" };
+
+    beforeEach(() => {
+      window.fetch.callsFake(() =>
+        Promise.resolve({ json: () => Promise.resolve(fakeData) })
+      );
+      let button = document.querySelector("#crawlBtn");
+      button.click();
+    });
+
     it('When the user clicks the button "Get Opening Crawl", you should fetch the data from the correct URL', () => {
-      expect(myVariable).to.eq(42)
-    })
+      expect(window.fetch).to.have.been.calledWith(
+        "https://swapi.co/api/films/1/"
+      );
+    });
 
-    it('When the promise is resolved, the opening crawl should appear on the page', () => {
-      expect(myVariable).to.eq(42)
-    })
-  })
+    it("When the promise is resolved, the opening crawl should appear on the page", done => {
+      setTimeout(() => {
+        let div = document.getElementById("crawlDiv");
+        expect(div.innerText).equal(fakeData.opening_crawl);
+        done();
+      }, 0);
+    });
+  });
 
-  describe('Star Wars Planets', () => {
+  describe("Star Wars Planets", () => {
     it("On submit of the planet form, fetch that planet's data from the correct url", () => {
-      expect(myFunction(9001)).to.be.true
-    })
+      expect(myFunction(9001)).to.be.true;
+    });
 
-    it('When the promise resolves, display the name and climate of the planet in the `#planetData`', () => {
-      expect(myFunction(42)).to.be.false
-    })
+    it("When the promise resolves, display the name and climate of the planet in the `#planetData`", () => {
+      expect(myFunction(42)).to.be.false;
+    });
 
-    it('Validates that the planet id is a number between 1 and 60', () => {
+    it("Validates that the planet id is a number between 1 and 60", () => {
       // multiple expects here
-      expect(myFunction(42)).to.be.false
-    })
-  })
+      expect(myFunction(42)).to.be.false;
+    });
+  });
 
-  describe('These Are The Droids', () => {
-    it('When the page loads, fetch the data for the characters', () => {
-      expect(myFunction(9001)).to.be.true
-    })
+  describe("These Are The Droids", () => {
+    it("When the page loads, fetch the data for the characters", () => {
+      expect(myFunction(9001)).to.be.true;
+    });
 
     it("When the promise resolves, display each droid's name, height, and mass on the screen in the appropriate divs", () => {
-      expect(myFunction(42)).to.be.false
-    })
+      expect(myFunction(42)).to.be.false;
+    });
 
     it("When the promise resolves, each droid div should have a button to 'Show Homeworld Details'", () => {
-      expect(myFunction(42)).to.be.false
-    })
+      expect(myFunction(42)).to.be.false;
+    });
 
     it('When the user clicks the button "Show Homeworld Details" for either droid, you should fetch the homeworld planet data from the correct URL', () => {
-      expect(myVariable).to.eq(42)
-    })
+      expect(myVariable).to.eq(42);
+    });
 
     it("When the promise resolves, each droid's div should display the homeworld name in a span", () => {
-      expect(myFunction(42)).to.be.false
-    })
-  })
-})
+      expect(myFunction(42)).to.be.false;
+    });
+  });
+});
 
-describe('numbers.js', () => {
-  describe('Number One', () => {
+describe("numbers.js", () => {
+  describe("Number One", () => {
     it("When a user clicks on the button 'Facts About 1', fetch a random fact about the number 1", () => {
-      expect(myVariable).to.eq(42)
-    })
-    it('When the promise is resolved, a random fact about the number 1 should be displayed in the `#one-facts` div', () => {
-      expect(myVariable).to.eq(42)
-    })
-  })
+      expect(myVariable).to.eq(42);
+    });
+    it("When the promise is resolved, a random fact about the number 1 should be displayed in the `#one-facts` div", () => {
+      expect(myVariable).to.eq(42);
+    });
+  });
 
-  describe('Pick a Number', () => {
-    it('On change of the number input, fetch a math fact about that number', () => {
-      expect(myFunction(9001)).to.be.true
-    })
-    it('When the promise is resolved, show the math fact on the screen in the `#random-math-fact` div', () => {
-      expect(myFunction(42)).to.be.false
-    })
-    it('Validates that the input is a number', () => {
-      expect(myFunction(42)).to.be.false
-    })
-  })
+  describe("Pick a Number", () => {
+    it("On change of the number input, fetch a math fact about that number", () => {
+      expect(myFunction(9001)).to.be.true;
+    });
+    it("When the promise is resolved, show the math fact on the screen in the `#random-math-fact` div", () => {
+      expect(myFunction(42)).to.be.false;
+    });
+    it("Validates that the input is a number", () => {
+      expect(myFunction(42)).to.be.false;
+    });
+  });
   // we aren't quite sure how to do this?
-  describe('Those who fail to study history', () => {
-    it('When the page loads, fetch a fact about this year', () => {
-      expect(myFunction(9001)).to.be.true
-    })
-    it('When the promise is resolved, the fact should be displayed in the `#year-history` div', () => {
-      expect(myFunction(9001)).to.be.true
-    })
+  describe("Those who fail to study history", () => {
+    it("When the page loads, fetch a fact about this year", () => {
+      expect(myFunction(9001)).to.be.true;
+    });
+    it("When the promise is resolved, the fact should be displayed in the `#year-history` div", () => {
+      expect(myFunction(9001)).to.be.true;
+    });
     // how do we test setInterval callback functions?
-    it('Every five seconds, the previous year`s fact should be fetched', () => {
-      expect(myFunction(9001)).to.be.true
-    })
-  })
+    it("Every five seconds, the previous year`s fact should be fetched", () => {
+      expect(myFunction(9001)).to.be.true;
+    });
+  });
 
-  describe('All the Numbers', () => {
-    it('When a user clicks the button, fetch facts for one hundred random numbers', () => {
-      expect(myFunction(9001)).to.be.true
-    })
-    it('When the promise is resolved, all facts should be displayed in the `#all-the-numbers` div', () => {
-      expect(myFunction(9001)).to.be.true
-    })
-  })
-})
+  describe("All the Numbers", () => {
+    it("When a user clicks the button, fetch facts for one hundred random numbers", () => {
+      expect(myFunction(9001)).to.be.true;
+    });
+    it("When the promise is resolved, all facts should be displayed in the `#all-the-numbers` div", () => {
+      expect(myFunction(9001)).to.be.true;
+    });
+  });
+});
